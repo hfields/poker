abb528d7d93d16455f2e2398833754179800c586
hfields <dks2@outlook.com>
2018-04-02 20:30:44 -0700

    Added Hand class to represent hands for any number of cards. Added __init__ and __repr__ methods for Hand class, as well as fillHand, a method for filling a hand with a certain number of cards, and findPair, which finds the highest value 5-card poker hands containing a pair that can be made with a hand and some amount of board cards. Also made countByValue function for determining the number of times a certain value appears in a list of cards, and pairTest, which returns the output of findPair on a randomly filled board of 5 cards and hand of 2 cards.

--- cards.py
+++ cards.py
@@ -2,6 +2,7 @@
 # 3/16/18
 
 import random
+import functools
 
 debug = True
 
@@ -52,7 +53,6 @@ class Card:
             return self.value < other.value
         else:
             return suits.index(self.suit) < suits.index(other.suit)
-
 
 
 class Deck:
@@ -107,9 +107,82 @@ class Deck:
             
             return randCards
 
-        
+    def burn(self):
+        """ Randomly burns a card, moving it from dealt to undealt 
+        without doing anything else with it."""
+        randCard = random.choice(self.undealt)
+        self.undealt.remove(randCard)
+        self.dealt += [randCard]
+
+class Hand:
+    """ Uses a list of Cards to represent a poker hand with the
+    designated number of cards. Contains methods for determining
+    the value of any 5-card hand that can be made from the hand's Cards
+    and some amount of Cards on the board."""
+    def __init__(self, numCards = 0, cards = []):      
+        self.cards = cards
+        self.numCards = numCards
+
+    def __repr__(self):
+        """ Returns a string representing the given hand"""
+        s = ""
+        for card in self.cards:
+            s += str(card) + "\n"
+        return s
+
+    def fillHand(self, deck):
+        """ Fills a hand with numCards cards from the given Deck"""
+        self.cards = deck.deal(self.numCards)
+
+    def findPair(self, board):
+        """ Returns all five card hands that can be made from the 
+        hand and the board containing one pair. Will not run if there
+        are not enough cards in the hand and the board to make 5"""
+        hands = []
+        usedValues = []
+        allCards = self.cards + board
+
+        # Exit if there are not enough cards
+        if len(allCards) < 5:
+            print("Not enough cards to make a poker hand.\n")
+            return
+
+        # Sort allCards from least to greatest
+        allCards.sort()
+
+        # For each card in allCards, check if it has a pair
+        for card in allCards:
+            if not card.value in usedValues:
+                # If so, add a 5-card hand containing that pair and the highest cards after it to hands
+                if countByValue(allCards, card.value) == 2:
+                    pair = list(filter(lambda x: x.value == card.value, allCards))
+                    filtCards = list(filter(lambda x: x.value != card.value, allCards))
+                    filtCards.reverse()
+                    hands += [pair + filtCards[0:3]]
+                    usedValues += [card.value]
+
+        return hands
+
+def countByValue(cards, value):
+    """ Returns a count of how many time a certain value appears in
+    a list of Cards"""
+    count = 0
+    for card in cards:
+        if card.value == value:
+            count += 1
+    return count
+
 def main():
     return 0
+
+def pairTest():
+    d = Deck()
+    board = d.deal(5)
+    print(board)
+    h = Hand(2)
+    h.fillHand(d)
+    print(h.cards)
+    return h.findPair(board)
 
 if __name__ == "__main__" and debug == False:
     main()
