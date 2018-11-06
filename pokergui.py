41639d99322079cb530150cb3f118eacea70a240
hfields <dks2@outlook.com>
2018-11-06 01:58:43 -0800

    Incorporated chip-tracking functionality into GUI. Added a table attribute to Application and changed __init__ function to take a Table object as an argument (instead of just numPlayers). Added attributes for playerNames, numPlayers, and startingChips for the sake of convenience, as well as chipCounts, round, and stopBetting to keep track of important variables tied to the progression of the game. A quit button has been added to close the application. The dealBoardButton and dealHandButton have been combined into the proceedButton (itself an attribute of Application) and a new method, proceed, has been added to progress the game state in all situations. dealBoard has been modified to include betting rounds (currently handled in the terminal). Each player's window is now represented by a Toplevel widget instead of a Tk widget, with the root Tk widget as each Toplevel's master. Added updateChips method to update the chip count variables of every player in the game.

--- pokergui.py
+++ pokergui.py
@@ -1,45 +1,235 @@
 from tkinter import *
 from cards import *
+from pokerchips import *
+from functools import *
 
 class Application():
-    def __init__(self, numPlayers, master, state = "preflop"):
+    def __init__(self, table, master, state = "preRound"):
         self.state = state
         self.master = master
-        self.numPlayers = numPlayers
+        self.table = table
+        self.playerNames = list(map(lambda x: x.name, table.allPlayers))
+        self.numPlayers = len(self.playerNames)
+        self.startingChips = table.allPlayers[0].chips
         self.deck = Deck()
         self.boardImages = []
         self.boardLabels = []
         self.faceupBoard = []
         self.handWindows = []
         self.hands = []
+        self.chipCounts = []
         self.handImages = {}
         self.handLabels = {}
+        self.round = 0
+        self.stopBetting = False
 
         # Add buttons and a face-down card to top
-        dealBoardButton = Button(top, 
-                        text="Deal to board", 
+        self.proceedButton = Button(self.master, 
+                        text="Proceed", 
                         fg="red",
-                        command=lambda: self.dealBoard()).pack()
+                        command=lambda: self.proceed())
+        self.proceedButton.pack()
 
-        dealHandButton = Button(top, 
-                        text="Deal to players", 
+        quitButton = Button(self.master, 
+                        text="Quit", 
                         fg="red",
-                        command=lambda: self.dealPlayers()).pack()
-
+                        command= self.master.destroy).pack(side = "bottom")
+    
         self.burnImage = PhotoImage(file = "Cards/Facedown.png")
         burnCard = Label(image=self.burnImage).pack(side = "left")
   
         # Create new windows for each player
         for i in range(0, self.numPlayers):
-            self.handWindows += [Tk()]
+            self.handWindows += [Toplevel()]
             self.handWindows[i].geometry("600x300")
-            playerString = "Player " + str(i + 1)
+            chipCount = StringVar()
+            self.chipCounts += [chipCount]
+            
+            chipCount.set(str(self.startingChips))
+
+            playerString = "Player " + str(i + 1) + ": " + self.playerNames[i]
             Label(self.handWindows[i], text = playerString).pack(side = "top")
+            Label(self.handWindows[i], textvariable = chipCount).pack(side = "top")
+
             Button(self.handWindows[i], 
                 text = "Flip cards",
                 fg = "red",
                 command=lambda i=i: self.flipCards(i)).pack()
+
+    def proceed(self):
+        """ Proceeds to the next step in the game, based on the current state and certain
+        table conditions"""
+        if self.state == "preRound":
+            print("PRE-FLOP")
+
+            # Disable Proceed button
+            self.proceedButton.config(state = DISABLED)
+
+            # Reset stopBetting and update state
+            self.stopBetting = False
+            self.state = "preflop"
+            
+            # Deal players and run a pre-flop betting rotation
+            self.dealPlayers()
+            table.getPreFlopRotation(self.round)
+            self.stopBetting = self.table.preflop()
+
+            # Re-enable the Proceed button and check to see if betting should stop
+            self.proceedButton.config(state = NORMAL)
+            self.stopBetting = self.stopBetting or self.table.allPlayersAllin()
+            
+            # If betting should stop, automatically proceed through dealing the cards
+            if self.stopBetting:
+                self.proceed()
+            
+            # Otherwise, prompt to proceed to the flop
+            else:
+                self.updateChips()
+                print("Pre-flop betting concluded. Press Proceed to deal flop")
         
+        else:
+            self.dealBoard()
+
+    def dealBoard(self):
+        """ Deals the community cards to the board"""
+        # Switch depending on the game state
+        if self.state == "preflop":
+            # Disable Proceed button
+            self.proceedButton.config(state = DISABLED)
+
+            # Burn one and deal 3 cards, then switch gamestate to flop
+            self.deck.burn()
+
+            flop = self.deck.deal(3)
+            self.faceupBoard += flop
+            self.addCards(flop)
+
+            self.state = "flop"
+
+            # If betting has already stopped, automatically proceed to the end
+            if self.stopBetting:
+                self.proceed()
+
+            else:
+                # Conduct flop betting
+                print("FLOP")
+                self.table.getRotation(self.round)
+
+                # Run a betting rotation
+                self.stopBetting = self.table.postflop()
+
+                # Re-enable the Proceed button and check to see if betting should continue
+                self.proceedButton.config(state = NORMAL)
+                self.stopBetting = self.stopBetting or self.table.allPlayersAllin()
+
+                # If betting should stop, automatically proceed to the end
+                if self.stopBetting:
+                    self.proceed()
+
+                else:
+                    self.updateChips()
+                    print("Flop betting concluded. Press Proceed to deal turn")
+
+        elif self.state == "flop":
+            # Disable Proceed button
+            self.proceedButton.config(state = DISABLED)
+
+            # Burn one and deal one card, then switch gamestate to turn
+            self.deck.burn()
+
+            turn = self.deck.deal(1)
+            self.faceupBoard += turn
+            self.addCards(turn)
+
+            self.state = "turn"
+
+            # If betting has already stopped, automatically proceed to the end
+            if self.stopBetting:
+                self.proceed()
+
+            else:
+                # Conduct turn betting
+                print("TURN")
+                self.table.getRotation(self.round)
+                
+                # Run a betting rotation
+                self.stopBetting = self.table.postflop()
+
+                # Re-enable the Proceed button and check to see if betting should continue
+                self.proceedButton.config(state = NORMAL)
+                self.stopBetting = self.stopBetting or self.table.allPlayersAllin()
+
+                # If betting should stop, automatically proceed to the end
+                if self.stopBetting:
+                    self.proceed()
+
+                else:
+                    self.updateChips()
+                    print("Turn betting concluded. Press Proceed to deal river")
+
+        elif self.state == "turn":
+            # Disable Proceed button
+            self.proceedButton.config(state = DISABLED)
+
+            # Burn one and deal one card, then switch gamestate to river
+            self.deck.burn()
+
+            river = self.deck.deal(1)
+            self.faceupBoard += river
+            self.addCards(river)
+
+            print(HandHelper.findWinner(self.hands, self.faceupBoard))
+
+            self.state = "river"
+
+            # If betting has already stopped, automatically proceed to the end
+            if self.stopBetting:
+                self.proceed()
+
+            else:
+                # Conduct river betting
+                print("RIVER")
+                self.table.getRotation(self.round)
+
+                # Run a betting rotation
+                self.table.postflop()
+
+                # Re-enable the Proceed button
+                self.proceedButton.config(state = NORMAL)
+
+                self.updateChips()
+                print("All betting concluded. Press Proceed to resolve pots.")
+
+        elif self.state == "river":
+            # Disable proceed button and resolve pots 
+            self.proceedButton.config(state = DISABLED)
+            self.table.resolvePots()
+
+            # Re-enable the Proceed button and switch gamestate to postRound
+            self.proceedButton.config(state = NORMAL)
+            self.state = "postRound"
+
+            self.updateChips()
+            print("Pots resolved. Press Proceed to go to next round.")
+
+        else:
+            # Reset the deck, increment round, clear the existing widgets and reset gamestate to preflop
+            self.round += 1
+            print("Round", self.round + 1)
+
+            self.deck.reset()
+
+            self.clearBoard()
+            self.clearHands()
+            self.faceupBoard = []
+
+            self.state = "preRound"
+
+    def updateChips(self):
+        """ Updates the chip counts of every player in the game."""
+        for i in range(0, len(self.table.allPlayers)):
+            self.chipCounts[i].set(str(self.table.allPlayers[i].chips))
+
     def clearBoard(self):
         """ Destroys all labels and images associated with the board and resets the
         boardImages and boardLabels lists to empty"""
@@ -94,58 +284,8 @@ class Application():
         for label in self.boardLabels[nextIndex:]:
             label.pack(side = "left")
 
-    def dealBoard(self):
-        """ Deals the community cards to the board"""
-
-        # Switch depending on the game state
-        if self.state == "preflop":
-            # Burn one and deal 3 cards, then switch gamestate to flop
-            print("Dealing the flop...")
-            self.deck.burn()
-
-            flop = self.deck.deal(3)
-            self.faceupBoard += flop
-            self.addCards(flop)
-
-            self.state = "flop"
-
-        elif self.state == "flop":
-            # Burn one and deal one card, then switch gamestate to turn
-            print("Dealing the turn...")
-            self.deck.burn()
-
-            turn = self.deck.deal(1)
-            self.faceupBoard += turn
-            self.addCards(turn)
-
-            self.state = "turn"
-
-        elif self.state == "turn":
-            # Burn one and deal one card, then switch gamestate to river
-            print("Dealing the river...")
-            self.deck.burn()
-
-            river = self.deck.deal(1)
-            self.faceupBoard += river
-            self.addCards(river)
-
-            print(HandHelper.findWinner(self.hands, self.faceupBoard))
-
-            self.state = "river"
-
-        else:
-            # Reset the deck, clear the existing widgets and reset gamestate to preflop
-            print("Resetting deck...")
-
-            self.deck.reset()
-
-            self.clearBoard()
-            self.clearHands()
-            self.faceupBoard = []
-
-            self.state = "preflop"
-
     def flipCards(self, playerIndex):
+        """ Flip the cards in a given player's window"""
         # If the cards are face-up, flip them face-down
         if self.hands[playerIndex].faceUp:
             # Delete the labels in this player's entry in self.handLabels
@@ -182,6 +322,7 @@ class Application():
         self.hands[playerIndex].flip()
 
     def dealPlayers(self):
+        """ Deal cards to the windows representing each Hand"""
         # Create hands for as many players as designated by numPlayers
         for i in range(0, self.numPlayers):
             self.hands += [Hand(2).fillHand(self.deck)]
@@ -203,10 +344,21 @@ class Application():
             for label in self.handLabels[i]:
                 label.pack(side = "left")
 
+# Initialize the poker table
+table = Table()
+
+# Get the players and blinds
+table.getPlayers()
+table.getBlinds()
+
 # Create the top level window
 top = Tk()
 top.geometry("1200x300")
 
-app = Application(2, top)
+app = Application(table, top)
+
+print("Round", app.round + 1)
 
 top.mainloop()
+
+del table
